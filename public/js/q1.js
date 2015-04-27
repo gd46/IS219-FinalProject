1013cde6a31b234d93d5075d426cea5b62988d78
Giuseppe DiBella <gd46@njit.edu>
2015-04-27 13:50:58 -0400

    Partial Charts

--- public/js/q1.js
+++ public/js/q1.js
@@ -1,53 +1,72 @@
 $(document).ready(function() {
-	
-	// $('#container').highcharts({
-	// 	    chart: {
-	// 	        type: 'bar'
-	// 	    },
-	// 	    title: {
-	// 	        text: 'Fruit Consumption'
-	// 	    },
-	// 	    xAxis: {
-	// 	        categories: ['Apples', 'Bananas', 'Oranges']
-	// 	    },
-	// 	    yAxis: {
-	// 	        title: {
-	// 	            text: 'Fruit eaten'
-	// 	        }
-	// 	    },
-	// 	    series: [{
-	// 	        name: 'Jane',
-	// 	        data: [1, 0, 4]
-	// 	    }, {
-	// 	        name: 'John',
-	// 	        data: [5, 7, 3]
-	// 	    }]
+	var collegeData;
+	$.ajax({
+	  url: "/api/q1",
+	  success : function(data){
+	  	console.log(data);
+	  	collegeData = data;
+	  	$('#container').highcharts({
+		    chart: {
+		        type: 'bar'
+		    },
+		    title: {
+		        text: 'Students'
+		    },
+		    xAxis: {
+		        categories: ['Apples', "Oranges", "Pairs"]
+		    },
+		    yAxis: {
+		        title: {
+		            text: 'Number of students'
+		        }
+		    },
+		    series: data
+
+		    //test.data
+		    // series: [{
+		    //     name: 'Males',
+		    //     //data: [test.students.male]
+		    //     data: [1200]
+		    // }, {
+		    //     name: 'Females',
+		    //     //data: [test.students.female]
+		    //     data: [500]
+		    // }]
+		});
+	  }
+	})
+	// var test = {
+	// 		data: [{
+	//             studentName: 'John',
+	//             data: [5, 3, 4]
+	//         }, {
+	//             studentName: 'Jane',
+	//             data: [2, 2, 3]
+	//         }, {
+	//             studentName: 'Joe',
+	//             data: [3, 4, 4]
+	//         }]
+	// 	};
+	// 	series = [],
+	// 	len = test.data.length,
+	// 	i = 0;
+	// for(i; i<len;i++){
+	// 	console.log(i);
+	// 	series.push({
+	// 		name: test.data[i].studentName,
+	// 		data: test.data[i].data
 	// 	});
+	// }
 
-	
-
-	
-
-	var test = {
-			data: [{
-	            studentName: 'John',
-	            data: [5, 3, 4]
-	        }, {
-	            studentName: 'Jane',
-	            data: [2, 2, 3]
-	        }, {
-	            studentName: 'Joe',
-	            data: [3, 4, 4]
-	        }]
-		};
-		series = [],
-		len = test.data.length,
+	console.log(collegeData);
+	var series = [],
+		len = collegeData.length,
 		i = 0;
 	for(i; i<len;i++){
 		console.log(i);
 		series.push({
-			name: test.data[i].studentName,
-			data: test.data[i].data
+			name: collegeData[i].name,
+			data: collegeData[i].enrollment
 		});
 	}
 		$('#container').highcharts({
