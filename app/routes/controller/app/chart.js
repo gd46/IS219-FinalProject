0eb702e7d2b8120de7a55c55d27d5df57dd20ab3
Binoy Patel <binoypatel14@gmail.com>
2015-04-27 13:38:15 -0400

    added routes for all questions

--- app/routes/controller/app/chart.js
+++ app/routes/controller/app/chart.js
@@ -1,21 +1,11 @@
-// var config = require('../../../../config'),
-//     College = require('../../../models/College');
-
-
-exports.q1 = function(req, res){
-	res.render('q1', {
-
-	});
+exports.q1 = function(req, res) {
+    res.render('q1');
 }
 
-exports.q2 = function(req, res){
-	res.render('q2', {
-
-	});
+exports.q2 = function(req, res) {
+    res.render('q2');
 }
 
-exports.q3 = function(req, res){
-	res.render('q3',{
-
-	});
+exports.q3 = function(req, res) {
+    res.render('q3');
 }
