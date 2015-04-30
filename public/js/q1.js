ee99bdafc901e41a26ad41e8c4c76fd781b79ec4
Giuseppe DiBella <gd46@njit.edu>
2015-04-30 13:43:09 -0400

    Got chart set up

--- public/js/q1.js
+++ public/js/q1.js
@@ -1,37 +1,41 @@
 $(document).ready(function() {
     var collegeData = [];
-    var enrollmentData = [];
     var collegeNames = [];
     $.ajax({
         url: "/api/q1",
         success: function(data) {
             async.forEach(data,
-                function(item, callback) {
-                    var jsonData = {};
-                    collegeNames.push(item.name);
-                    enrollmentData.push(item.enrollment);
-                    jsonData['name'] = item.name;
-                    jsonData.push(enrollmentData);
+                function(college, callback) {
+                    collegeData.push({
+                        name: college.name,
+                        data: college.enrollment
+                    });
+                    collegeNames.push({
+                        name: college.name
+                    });
                     callback();
                 },
                 function(err) {
                     if (err) throw err;
-
+                    console.log(collegeNames.name);
+                    $('#container').highcharts({
+                        chart: {
+                            type: 'bar'
+                        },
+                        title: {
+                            text: 'Top 10 colleges by enrollment'
+                        },
+                        xAxis: {
+                            categories: collegeNames.name
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'Colleges'
+                            }
+                        },
+                        series: collegeData
+                    });
                 });
-            /*$('#container').highcharts({
-                chart: {
-                    type: 'bar'
-                },
-                title: {
-                    text: 'Students'
-                },
-                yAxis: {
-                    title: {
-                        text: 'Number of students'
-                    }
-                },
-                series: data
-            });*/
         }
     })
 });
