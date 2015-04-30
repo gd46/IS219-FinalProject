411a9c02653619d1121f88e25ed010c781699018
Binoy Patel <binoypatel14@gmail.com>
2015-04-30 13:58:12 -0400

    Charts for q1

--- public/js/q1.js
+++ public/js/q1.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
     var collegeData = [];
     var collegeNames = [];
+    var finalData = [];
     $.ajax({
         url: "/api/q1",
         success: function(data) {
             async.forEach(data,
                 function(college, callback) {
-                    collegeData.push({
-                        name: college.name,
-                        data: college.enrollment
-                    });
-                    collegeNames.push({
-                        name: college.name
-                    });
+                    collegeData.push(college.enrollment);
+                    collegeNames.push(college.name);
                     callback();
                 },
                 function(err) {
                     if (err) throw err;
-                    console.log(collegeNames.name);
+                    finalData.push({
+                        name: 'Enrollments',
+                        data: collegeData
+                    });
                     $('#container').highcharts({
                         chart: {
                             type: 'bar'
@@ -25,15 +24,22 @@ $(document).ready(function() {
                         title: {
                             text: 'Top 10 colleges by enrollment'
                         },
+                        plotOptions: {
+                            bar: {
+                                dataLabels: {
+                                    enabled: true
+                                }
+                            }
+                        },
                         xAxis: {
-                            categories: collegeNames.name
+                            categories: collegeNames
                         },
                         yAxis: {
                             title: {
                                 text: 'Colleges'
                             }
                         },
-                        series: collegeData
+                        series: finalData
                     });
                 });
         }
