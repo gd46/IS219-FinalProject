63902798352959eaabea18d9ce3cbfbd667cdc66
Binoy Patel <binoypatel14@gmail.com>
2015-04-30 16:58:11 -0400

    Q2 completed with pie charts

--- public/js/q2.js
+++ public/js/q2.js
@@ -1,63 +1,41 @@
 $(document).ready(function() {
-    var collegeData = [];
-    var collegeNames = [];
-    var males = [];
-    var females = [];
+    var males = ['Males'];
+    var females = ['Females'];
+    var param = location.href;
+    param = param.match(/\/(\d+)/);
+    param = param[1];
     $.ajax({
-        url: "/api/q2:id",
+        url: "/api/q2/" + param,
         success: function(data) {
-            async.forEach(data,
-                function(college, callback) {
-                    console.log(data);
-                    collegeData.push(college.enrollment);
-                    collegeNames.push(college.name);
-                    callback();
+            males.push(data.EFTOTLM);
+            females.push(data.EFTOTLW);
+            $('#container').highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
                 },
-                function(err) {
-                    if (err) throw err;
-                    $('#container').highcharts({
-                        chart: {
-                            plotBackgroundColor: null,
-                            plotBorderWidth: null,
-                            plotShadow: false
-                        },
-                        title: {
-                            text: 'Enrollment breakdown'
-                        },
-                        // tooltip: {
-                        //     pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                        // },
-                        // plotOptions: {
-                        //     pie: {
-                        //         allowPointSelect: true,
-                        //         cursor: 'pointer',
-                        //         dataLabels: {
-                        //             enabled: true,
-                        //             format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                        //             style: {
-                        //                 color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                        //             }
-                        //         }
-                        //     }
-                        // },
-                        series: [{
-                            type: 'pie',
-                            name: 'Enrollment Breakdown',
-                            data: [
-                                ['Firefox', 45.0],
-                                ['IE', 26.8], {
-                                    name: 'Chrome',
-                                    y: 12.8,
-                                    sliced: true,
-                                    selected: true
-                                },
-                                ['Safari', 8.5],
-                                ['Opera', 6.2],
-                                ['Others', 0.7]
-                            ]
-                        }]
-                    });
-                });
+                title: {
+                    text: data.INSTNM + ' Enrollment breakdown'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            }
+                        }
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    name: 'Enrollment Breakdown',
+                    data: [males, females]
+                }]
+            });
         }
     })
 });
